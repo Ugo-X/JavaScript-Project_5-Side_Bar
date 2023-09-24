9268a8a13e7f10ad57ded7e1973b785fa547887c
Ugo-X <ugonnadike001@gmail.com>
2023-09-24 20:25:44 +0100

    updated app.js

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
 
 
+const sidebar = document.querySelector(".sidebar");
+const closeBtn = document.querySelector(".close-btn");
+const sideBarBtn = document.querySelector(".sidebar-toggle");
 
+sideBarBtn.addEventListener("click", function () {
+  sidebar.classList.toggle("show-sidebar");
+});
+
+closeBtn.addEventListener("click", function () {
+  sidebar.classList.remove("show-sidebar"); // remove show class from the element
+});
